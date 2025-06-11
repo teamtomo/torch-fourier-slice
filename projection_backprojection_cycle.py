0738002f54950aa8d1c27cb01d8ede49c1ade293
McHaillet <martenchaillet@gmail.com>
2025-06-11 11:35:54 +0200

    update to pad_factor in example code

--- projection_backprojection_cycle.py
+++ projection_backprojection_cycle.py
@@ -11,7 +11,9 @@ N_IMAGES = 1000
 torch.manual_seed(42)
 
 # load a volume and normalise
-volume = torch.tensor(mrcfile.read("/Users/burta2/data/4v6x_bin4.mrc"))
+volume = torch.tensor(
+    mrcfile.read("/home/marten/data/datasets/emdb/test/emd_48372_10A.mrc")
+)
 volume -= torch.mean(volume)
 volume /= torch.std(volume)
 
@@ -24,23 +26,21 @@ rotations = torch.tensor(
 projections = project_3d_to_2d(
     volume,
     rotation_matrices=rotations,
-    pad=True,
+    pad_factor=3.0,
 )  # (b, h, w)
 
 # reconstruct volume from projections
 reconstruction = backproject_2d_to_3d(
     images=projections,
     rotation_matrices=rotations,
-    pad=True,
+    pad_factor=3.0,
 )
 reconstruction -= torch.mean(reconstruction)
 reconstruction = reconstruction / torch.std(reconstruction)
 
 # visualise
-viewer = napari.Viewer()
+viewer = napari.Viewer()  # ndisplay=3)
 viewer.add_image(projections.numpy(), name="projections")
-
-viewer = napari.Viewer(ndisplay=3)
 viewer.add_image(volume.numpy(), name="ground truth")
 viewer.add_image(reconstruction.numpy(), name="reconstruction")
 napari.run()
