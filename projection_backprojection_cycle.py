9a28ab8270bb58ab8b5d31f4e43229c14bdde8df
McHaillet <martenchaillet@gmail.com>
2025-06-12 11:25:51 +0200

    add fsc example with pad_factor

--- projection_backprojection_cycle.py
+++ projection_backprojection_cycle.py
@@ -1,13 +1,15 @@
 """A simple projection/backprojection cycle implementation."""
 
+import matplotlib.pyplot as plt
 import mrcfile
 import napari
 import torch
 from scipy.stats import special_ortho_group
+from torch_fourier_shell_correlation import fsc
 
 from torch_fourier_slice import backproject_2d_to_3d, project_3d_to_2d
 
-N_IMAGES = 1000
+N_IMAGES = 50
 torch.manual_seed(42)
 
 # load a volume and normalise
@@ -22,25 +24,34 @@ rotations = torch.tensor(
     special_ortho_group.rvs(dim=3, size=N_IMAGES, random_state=42)
 ).float()
 
-# make projections
-projections = project_3d_to_2d(
-    volume,
-    rotation_matrices=rotations,
-    pad_factor=3.0,
-)  # (b, h, w)
+fig, ax = plt.subplots()
 
-# reconstruct volume from projections
-reconstruction = backproject_2d_to_3d(
-    images=projections,
-    rotation_matrices=rotations,
-    pad_factor=3.0,
-)
-reconstruction -= torch.mean(reconstruction)
-reconstruction = reconstruction / torch.std(reconstruction)
+for x in [1.0, 1.5, 2.0, 3.0, 4.0]:
+    # make projections
+    projections = project_3d_to_2d(
+        volume,
+        rotation_matrices=rotations,
+        pad_factor=x,
+    )  # (b, h, w)
+
+    # reconstruct volume from projections
+    reconstruction = backproject_2d_to_3d(
+        images=projections,
+        rotation_matrices=rotations,
+        pad_factor=x,
+    )
+    reconstruction -= torch.mean(reconstruction)
+    reconstruction = reconstruction / torch.std(reconstruction)
+
+    _fsc = fsc(volume, reconstruction)[1:]
+    ax.plot(_fsc, label=f"{x}")
+
+ax.legend()
+plt.show()
 
 # visualise
-viewer = napari.Viewer()  # ndisplay=3)
-viewer.add_image(projections.numpy(), name="projections")
+viewer = napari.Viewer()
+# viewer.add_image(projections.numpy(), name="projections")
 viewer.add_image(volume.numpy(), name="ground truth")
 viewer.add_image(reconstruction.numpy(), name="reconstruction")
 napari.run()
