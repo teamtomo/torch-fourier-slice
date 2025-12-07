b2f427c00b7e5566ab0b5ced7bd716c42b202ec1
jdickerson95 <jdickerson@mrc-lmb.cam.ac.uk>
2025-12-07 14:40:15 -0800

    Add Ewald sphere curvature support

--- src/torch_fourier_slice/_grids/__init__.py
+++ src/torch_fourier_slice/_grids/__init__.py
@@ -1,7 +1,10 @@
 from ._central_line_fftfreq_grid import _central_line_fftfreq_grid
 from ._central_slice_fftfreq_grid import _central_slice_fftfreq_grid
+from ._ewald_curvature import _calculate_ewald_z, _apply_ewald_curvature
 
 __all__ = [
     "_central_slice_fftfreq_grid",
     "_central_line_fftfreq_grid",
+    "_calculate_ewald_z",
+    "_apply_ewald_curvature",
 ]
