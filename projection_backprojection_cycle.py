b97e8b0cb28ad635beb79e877eb99c05cd6489c1
McHaillet <martenchaillet@gmail.com>
2025-12-12 09:52:21 +0100

    set to py3.12 to get ruff to stop complaining"

--- projection_backprojection_cycle.py
+++ projection_backprojection_cycle.py
@@ -14,19 +14,20 @@ torch.manual_seed(42)
 
 # load a volume and normalise
 volume = torch.tensor(
-    mrcfile.read("/home/marten/data/datasets/emdb/test/emd_48372_10A.mrc")
+    mrcfile.read("/home/marten/data/datasets/emdb/test/emd_48372_10A.mrc"),
+    device="cuda",
 )
 volume -= torch.mean(volume)
 volume /= torch.std(volume)
 
 # rotation matrices for projection (operate on xyz column vectors)
 rotations = torch.tensor(
-    special_ortho_group.rvs(dim=3, size=N_IMAGES, random_state=42)
+    special_ortho_group.rvs(dim=3, size=N_IMAGES, random_state=42), device="cuda"
 ).float()
 
 fig, ax = plt.subplots()
 
-for x in [1.0, 1.5, 2.0, 3.0, 4.0]:
+for x in [1.0, 1.5, 2.0]:  # , 3.0, 4.0]:
     # make projections
     projections = project_3d_to_2d(
         volume,
@@ -43,7 +44,7 @@ for x in [1.0, 1.5, 2.0, 3.0, 4.0]:
     reconstruction -= torch.mean(reconstruction)
     reconstruction = reconstruction / torch.std(reconstruction)
 
-    _fsc = fsc(volume, reconstruction)
+    _fsc = fsc(volume.to("cpu"), reconstruction.to("cpu"))
     ax.plot(_fsc, label=f"{x}")
 
 ax.legend()
@@ -52,6 +53,6 @@ plt.show()
 # visualise
 viewer = napari.Viewer()
 # viewer.add_image(projections.numpy(), name="projections")
-viewer.add_image(volume.numpy(), name="ground truth")
-viewer.add_image(reconstruction.numpy(), name="reconstruction")
+viewer.add_image(volume.cpu().numpy(), name="ground truth")
+viewer.add_image(reconstruction.cpu().numpy(), name="reconstruction")
 napari.run()
