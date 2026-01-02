9c3409b46ed93cb77fc218153265e08afe4805f1
Marten Chaillet <58044494+McHaillet@users.noreply.github.com>
2026-01-02 17:28:02 +0100

    Fix friedel symmetry slice insertion (#44)
    * set to py3.12 to get ruff to stop complaining"
    
    * add mask for the redundant part of the x=0 line and slice so we don't insert those values twice
    
    * add custom version for image interpolation
    
    * add test for friedel symmetry and update code for friedel symmetric insertion on the x=0 plane"
    
    * add device switch
    
    * directly use 3d dft from insert_central_slices
    
    * simplify test
    
    * don't assign unused var'
    
    * use expand to return views to the data instead of copies (as done by repeat in einops)
    
    * rename to unique function names for fourier space insertion
    
    * add 0 and 180 example
    
    * change criterium
    
    * remove unuseful claude comments

--- projection_backprojection_cycle.py
+++ projection_backprojection_cycle.py
@@ -11,22 +11,24 @@ from torch_fourier_slice import backproject_2d_to_3d, project_3d_to_2d
 
 N_IMAGES = 1000
 torch.manual_seed(42)
+device = "cpu"
 
 # load a volume and normalise
 volume = torch.tensor(
-    mrcfile.read("/home/marten/data/datasets/emdb/test/emd_48372_10A.mrc")
+    mrcfile.read("/home/marten/data/datasets/emdb/test/emd_48372_10A.mrc"),
+    device=device,
 )
 volume -= torch.mean(volume)
 volume /= torch.std(volume)
 
 # rotation matrices for projection (operate on xyz column vectors)
 rotations = torch.tensor(
-    special_ortho_group.rvs(dim=3, size=N_IMAGES, random_state=42)
+    special_ortho_group.rvs(dim=3, size=N_IMAGES, random_state=42), device=device
 ).float()
 
 fig, ax = plt.subplots()
 
-for x in [1.0, 1.5, 2.0, 3.0, 4.0]:
+for x in [1.0, 1.5, 2.0]:  # , 3.0, 4.0]:
     # make projections
     projections = project_3d_to_2d(
         volume,
@@ -43,7 +45,7 @@ for x in [1.0, 1.5, 2.0, 3.0, 4.0]:
     reconstruction -= torch.mean(reconstruction)
     reconstruction = reconstruction / torch.std(reconstruction)
 
-    _fsc = fsc(volume, reconstruction)
+    _fsc = fsc(volume.to("cpu"), reconstruction.to("cpu"))
     ax.plot(_fsc, label=f"{x}")
 
 ax.legend()
@@ -52,6 +54,6 @@ plt.show()
 # visualise
 viewer = napari.Viewer()
 # viewer.add_image(projections.numpy(), name="projections")
-viewer.add_image(volume.numpy(), name="ground truth")
-viewer.add_image(reconstruction.numpy(), name="reconstruction")
+viewer.add_image(volume.cpu().numpy(), name="ground truth")
+viewer.add_image(reconstruction.cpu().numpy(), name="reconstruction")
 napari.run()
